4c07628f0cf8dee460390b8af7e2597dfdb615f6
Dustin Silveri <45640599+dustinsilveri@users.noreply.github.com>
2021-03-12 13:09:32 -0800

    Update daily-text-AWS-email-v2.py

--- daily-text-AWS-email-v2.py
+++ daily-text-AWS-email-v2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 ##################################################################
-## Created by van6uard
+## Created by d_silv
 ## 
 ## This script will scrape the daily text off wol.jw.org and email it.  
 ## setup this up on a cron job to receive the daily text everyday.
